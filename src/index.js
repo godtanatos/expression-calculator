1af82ea560df09571c631fbb9cae7ef849e99e68
godtanatos <78075671+godtanatos@users.noreply.github.com>
2021-08-17 22:34:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let balance = 0;
+    let reg = /\/ 0/g;
+    expr.split("").forEach(element => {
+        if (element == "(") {
+            balance++;
+        } else if (element == ")") {
+            balance--;
+        }
+
+    });
+    if (balance != 0) {
+        throw new Error('ExpressionError: Brackets must be paired')
+    }
+
+    let sum = new Function(`return ${expr}`);
+
+    p = sum();
+    if (expr.search(reg) !== -1) {
+        throw new Error("TypeError: Division by zero.");
+    } else
+        return p;
 }
 
 module.exports = {
